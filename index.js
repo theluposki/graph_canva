d01be9ff6d708d5f269d3cf62c1d7aa5defc0eda
theluposki <theluposki@gmail.com>
2024-12-29 16:02:01 -0300

    j

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import GraphBarra from "./graph_barra.js";
 import { gerarArrayAleatorio } from "./util.js";
 
+const range = document.getElementById("range");
+const value_range = document.querySelector(".value_range");
+
+value_range.innerHTML = range.value;
+
 const meta = 30;
-const dados = gerarArrayAleatorio(100, meta)
+let dados = gerarArrayAleatorio(100, meta);
 
-const graf = new GraphBarra('tela', dados, 30);
+const graf = new GraphBarra("tela", dados, 30);
 
-graf.loop()
+graf.draw();
 
-
-
+range.addEventListener("input", (event) => {
+  value_range.innerHTML = event.target.value;
+  dados = gerarArrayAleatorio(event.target.value, meta);
+  const graf = new GraphBarra("tela", dados, 30);
+  graf.draw();
+});
